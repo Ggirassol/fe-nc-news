a86b5560b8573c4a16482a4fe59a92dba7cb491c
Ggirassol <gi-12@hotmail.com>
2024-03-12 14:40:42 +0000

    View a list of comments associated with an article

--- src/components/SingleArticle/SingleArticle.jsx
+++ src/components/SingleArticle/SingleArticle.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getSingleArticle } from "../../../api";
+import { getSingleArticle, getArticleComments } from "../../../api";
 import "./SingleArticle.css"
 
 const SingleArticle = () => {
 
     const [currSingleArticle, setCurrSingleArticle] = useState({})
     const [isLoading, setIsLoading] = useState(true)
+    const [currComments, setCurrComents] = useState([])
 
     const {article_id} = useParams();
     useEffect(() => {
@@ -16,9 +17,17 @@ const SingleArticle = () => {
             setIsLoading(false)
         })
     }, [])
+
+    useEffect(() => {
+        getArticleComments(article_id).then(({comments}) => {
+            setCurrComents(comments)
+        })
+    }, [])
     
-    return isLoading ?
-        (<p className="loading">Loading article...</p>) : (
+    return isLoading ? (
+      <p className="loading">Loading article...</p>
+    ) : (
+      <>
         <div className="single-article">
         <h1>{currSingleArticle.title}</h1>
         <img src={currSingleArticle.article_img_url}/>
@@ -27,7 +36,29 @@ const SingleArticle = () => {
         <h3>{currSingleArticle.created_at ? currSingleArticle.created_at.slice(0, 10) : ""}</h3>
         <p>{currSingleArticle.votes} Likes</p>
         </div>
-    )
+        <div className="comments">
+          <h2>Comments</h2>
+          <ul>
+            {currComments.map((comment) => {
+              return (
+                <li key={comment.comment_id}>
+                  <p>{comment.body}</p>
+                  <div className="space-between">
+                    <span className="likes">{comment.votes} Likes</span>
+                    <div className="author-date">
+                      <span className="author">{comment.author} </span>
+                      <span className="date">
+                        {comment.created_at.slice(0, 10)}
+                      </span>
+                    </div>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </>
+    );
 
 }
 
